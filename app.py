4f701bb9de547d699f258c9d920692177a2b6d03
Satyam2006chh <satyam3396.beai24@chitkara.edu.in>
2025-06-09 06:47:54 +0530

    Add Streamlit app with title and text input area

--- app.py
+++ app.py
@@ -1,2 +1,19 @@
 import streamlit as st
 from fpdf import FPDF
+
+st.title("Text-to-PDF Converter")
+
+user_text = st.text_area("Enter Your Text Here: ",height=340)
+if st.button("Convert To PDF"):
+    if user_text.strip() ==" ":
+        st.warning("Please Enter some Text First !!")
+    else:
+        pdf =FPDF()
+        pdf.add_page()
+        pdf.set_font("Arial",size=12)
+        for line in user_text.split('\n'):
+            safe_line = ''.join(c for c in line if ord(c) < 128)
+            pdf.multi_cell(0, 10, safe_line)
+        pdf.output("generated.pdf")
+
+
